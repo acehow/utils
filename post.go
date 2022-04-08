7ddc48705cbfa82720b8c247128a9087f5f0af92
acehow <wuhaonet@msn.com>
2022-04-08 10:09:12 +0800

    commit
    commit

---
+++ post.go
@@ -0,0 +1,73 @@
+package utils
+var client = http.Client{
+	Timeout: 10 * time.Second,
+}
+
+func HttpPostJson(url string, data interface{}, result interface{}, header map[string]string) error {
+	buf := bytes.NewBuffer(nil)
+	encoder := json.NewEncoder(buf)
+	if err := encoder.Encode(data); err != nil {
+		return err
+	}
+
+	request, err := http.NewRequest(http.MethodPost, url, buf)
+	if err != nil {
+		return err
+	}
+
+	request.Header.Add("Content-Type", "application/json")
+	if header != nil {
+		for k, v := range header {
+			request.Header.Add(k, v)
+		}
+	}
+
+	response, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	decoder := json.NewDecoder(response.Body)
+	if err = decoder.Decode(&result); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func HttpPostForm(posturl string, data url.Values, result interface{}, host string) error {
+	request, err := http.NewRequest(http.MethodPost, posturl, strings.NewReader(data.Encode()))
+	if err != nil {
+		return err
+	}
+
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	//request.Host = host
+
+	resp, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	decoder := json.NewDecoder(resp.Body)
+	if err = decoder.Decode(&result); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func HttpGetRequest(url string, result interface{}) error {
+	resp, err := client.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode(&result)
+
+	return err
+}
