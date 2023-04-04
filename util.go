6ad724c0640ee2bc1b83a72242879a414a3a1675
acehow <wuhaonet@msn.com>
2023-04-04 15:20:10 +0800

    add StrMul function

--- util.go
+++ util.go
@@ -34,6 +34,34 @@ func StrMod(num string, k int) (int,error) {
 	return left,nil
 }
 
+// get num1*num2 as string
+func StrMul(num1 string, num2 string) string {
+	if num1 == "0" || num2 == "0" {
+		return "0"
+	}
+
+	sumArr := make([]int, len(num1)+len(num2))
+
+	for i := len(num2) - 1; i >= 0; i-- {
+		n2 := int(num2[i] - '0')
+		for j := len(num1) - 1; j >= 0; j-- {
+			n1 := int(num1[j] - '0')
+			sum := n2*n1 + sumArr[i+j+1]
+			sumArr[i+j+1] = sum % 10
+			sumArr[i+j] += sum / 10
+		}
+	}
+
+	res := ""
+	for k, v := range sumArr {
+		if k == 0 && v == 0 {
+			continue
+		}
+		res += string(v + '0')
+	}
+	return res
+}
+
 // get random element from slice
 func GetRandElem[T any](count int, data []T, isCopy bool) []T {
 	if len(data) < count {
